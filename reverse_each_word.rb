2e8c792d87de09c8deb986995d61959897340191
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-05 04:20:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 def reverse_each_word(string) 
   words = string.split(" ")
-  words_reversed = ""
+  words_reversed = []
   
   words.each do |word|
-    words_reversed += word.reverse
+    words_reversed << word.reverse
   end  
   
-  return words_reversed
+  return words_reversed.join(" ")
 end
 
 reverse_each_word("Hello there, and how are you?")
